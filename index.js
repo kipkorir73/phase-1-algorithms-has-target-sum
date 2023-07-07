4b817f4012bad78891f0968218c10c637e15ef5d
collins <kipkorirc583@gmail.com>
2023-07-07 21:17:26 +0300

    First commit

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  // Pseudocode:
+  // 1. Create an empty object to store seen numbers
+  // 2. Iterate through the array
+  //    - For each number, calculate the complement (target - current number)
+  //    - Check if the complement exists in the seen numbers
+  //    - If it exists, return true
+  //    - If it doesn't exist, add the current number to the seen numbers
+  // 3. If the loop completes without finding a pair, return false
+
+  const seen = {};
+
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    if (seen[complement]) {
+      return true;
+    }
+    seen[array[i]] = true;
+  }
+
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
-*/
+  Time complexity: O(n), where n is the length of the array.
+  The function iterates through the array once, performing constant-time operations for each element.
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
+  Explanation:
+  The `hasTargetSum` function uses a hash table (object) to keep track of seen numbers. It iterates through the array, calculating the complement for each number and checking if the complement exists in the hash table. If the complement is found, it means there is a pair of numbers that sums up to the target. If the loop completes without finding a pair, it means there is no such pair in the array.
 */
 
 // You can run `node index.js` to view these console logs
